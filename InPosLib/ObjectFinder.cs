a5edd5558ac06af0a94688464de8ecffaca96142
tsaltd <wp710012@gmail.com>
2016-01-19 15:50:05 -0500

    Revert "new algorithm"
    This reverts commit a66f9edcf515957f9733851014f26832769a16df.

--- InPosLib/ObjectFinder.cs
+++ InPosLib/ObjectFinder.cs
@@ -11,36 +11,25 @@ namespace InPosLib
         public List<object> searchThisList = new List<object>();
         public object typeToFind;
         public bool found;
-        List<int> locations = new List<int>();
         public ObjectFinder(List<object> thelist, object theObj)
         {
-            List<object> searchThisList = new List<object>();
             searchThisList = thelist;
-            object typeToFind = new object();
             typeToFind = theObj;
+            //var wtf = searchThisList(1)
 
-            public bool In()
+            foreach (var item in searchThisList)
             {
-                object result = thelist.Find(x => typeof(x) == typeToFind );
-            }
-
-            //foreach (var item in searchThisList)
-            //{
-            //    int here = searchThisList.FindIndex(x => (item.GetType() == typeToFind.GetType()));
-            //    //int here = searchThisList.FindIndex(x => (item.GetType() == typeToFind.GetType()));
-            //    locations.Add(here);
-                
-            //    //if (item.GetType() == typeToFind.GetType())
-            //    //    found = true;
-            //    ////int where = 
-            //    ////    locations.Add(indexof,)
-
-            //    //   // int oddIndex = elements.FindIndex(x => x % 2 != 0);
-                    
+                if (item.GetType() == typeToFind.GetType())
+                    found = true;
 
             }
-        }
-    }
             
+        }
 
+            //public bool In()
+            //{
+            //    if (typeToFind.GetType() == typeof()) { }
+            //}
 
+        }
+}
