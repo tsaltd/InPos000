a66f9edcf515957f9733851014f26832769a16df
tsaltd <wp710012@gmail.com>
2016-01-19 15:49:35 -0500

    new algorithm

--- InPosLib/ObjectFinder.cs
+++ InPosLib/ObjectFinder.cs
@@ -11,25 +11,36 @@ namespace InPosLib
         public List<object> searchThisList = new List<object>();
         public object typeToFind;
         public bool found;
+        List<int> locations = new List<int>();
         public ObjectFinder(List<object> thelist, object theObj)
         {
+            List<object> searchThisList = new List<object>();
             searchThisList = thelist;
+            object typeToFind = new object();
             typeToFind = theObj;
-            //var wtf = searchThisList(1)
 
-            foreach (var item in searchThisList)
+            public bool In()
             {
-                if (item.GetType() == typeToFind.GetType())
-                    found = true;
+                object result = thelist.Find(x => typeof(x) == typeToFind );
+            }
+
+            //foreach (var item in searchThisList)
+            //{
+            //    int here = searchThisList.FindIndex(x => (item.GetType() == typeToFind.GetType()));
+            //    //int here = searchThisList.FindIndex(x => (item.GetType() == typeToFind.GetType()));
+            //    locations.Add(here);
+                
+            //    //if (item.GetType() == typeToFind.GetType())
+            //    //    found = true;
+            //    ////int where = 
+            //    ////    locations.Add(indexof,)
+
+            //    //   // int oddIndex = elements.FindIndex(x => x % 2 != 0);
+                    
 
             }
+        }
+    }
             
-        }
 
-            //public bool In()
-            //{
-            //    if (typeToFind.GetType() == typeof()) { }
-            //}
 
-        }
-}
