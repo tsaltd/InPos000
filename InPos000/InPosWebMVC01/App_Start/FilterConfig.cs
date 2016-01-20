43e016b9c8a758717f5b97fcadff1e1de536c712
tsaltd <wp710012@gmail.com>
2016-01-20 14:48:03 -0500

    ReadyToDeploy

---
+++ InPos000/InPosWebMVC01/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace InPosWebMVC01
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ InPos000/InPosWebMVC01/Controllers/InPosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InPosWebMVC01.Controllers
+{
+    public class InPosController : Controller
+    {
+        // GET: InPos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
