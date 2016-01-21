8f523ad0d287edceeb398dd354ad20710f006f56
tsaltd <wp710012@gmail.com>
2016-01-21 00:33:26 -0500

    Iteration_02_Start

---
+++ InPos000/InPos000Data/InPosViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InPos000Data
+{
+    class InPosViewModel
+    {
+        public string me{ get; set; }
+    }
+}
