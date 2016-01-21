ae5b67abf1627429d9fd81e99b691c6a7e08bb5d
tsaltd <wp710012@gmail.com>
2016-01-21 01:15:42 -0500

    fix this

--- InPos000/InPos000Data/InPosLookups.cs
+++ InPos000/InPos000Data/InPosLookups.cs
@@ -11,6 +11,16 @@ namespace InPos000Data
             public int Id { get; set; }
             public string Name{ get; set; }
     }
+    public class Employee
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+        public float Salary { get; set; }
+
+
+    }
 
 
         
